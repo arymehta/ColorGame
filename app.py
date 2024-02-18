79a2e4698ff2aa3a571e0ce0045e469368a9a3bd
arymehta <141386426+arymehta@users.noreply.github.com>
2024-02-18 20:26:26 +0530

    Added Random Colour generation

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, redirect, request
+from random import randint
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     if request.method == "GET":
-        return render_template("index.html")
+        R = randint(0, 255)
+        G = randint(0, 255)
+        B = randint(0, 255)
+
+        return render_template("index.html", R = R, G = G, B = B)
 
     
 
